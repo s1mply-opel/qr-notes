be974c47d9b3d9af081a239c5856aed54ea8c981
s1mply <hsuowen7@gmail.com>
2023-06-20 22:58:17 +0800

    installed native-base

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider, TextArea, Checkbox, Heading} from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <TextArea placeholder="Text Area Placeholder"/>
+      
+      <Checkbox  value="checkbox">
+        Hi
+      </Checkbox>
+      
+      <Heading >Heading Text</Heading>
+      
+
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
