f6f1530afa3a638eada41a02d88db9e7055d920b
s1mply <hsuowen7@gmail.com>
2023-07-28 21:14:54 +0700

    Working version

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -2,14 +2,17 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "../screens/HomeScreen";
+import ListScreen from "../screens/ListScreen";
+import TestScreen from "../screens/TestScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppStack (){
     return(
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-            <Stack.Screen name="Main" component={HomeScreen}/>
-            
+        <Stack.Navigator screenOptions={{headerShown: true}}>
+            {/* <Stack.Screen name="Main" component={HomeScreen}/>
+            <Stack.Screen name="List" component={ListScreen}/> */}
+            <Stack.Screen name="Test" component={TestScreen}/>
         </Stack.Navigator>
     );
 };
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,14 +1,45 @@
 import React, { useState } from 'react';
-import { Text, View } from 'native-base';
+import { Text, View, NativeBaseProvider} from 'native-base';
+import QRCode from 'qrcode';
+import Svg, { Circle, Rect } from 'react-native-svg';
 
 export default function ListScreen(){
     const [entriesList, setEntriesList] = useState([]);
-    
+    const [svgString, setSVGString] = useState('');
+
+    const t = "This is owen";
+
+    let a = 'test';
+
+    QRCode.toString(t, (err, string) => {
+        if(err) throw err;
+        a = string;
+        // console.log(string);
+    });
+
     return(
-        <View>
-            <Text>
-                This is List Screen
-            </Text>
-        </View>
+        <NativeBaseProvider>
+            <View>
+                <Svg height="50%" width="50%" viewBox="0 0 100 100">
+                    <Circle
+                        cx="50"
+                        cy="50"
+                        r="45"
+                        stroke="blue"
+                        strokeWidth="2.5"
+                        fill="green"
+                    />
+                    <Rect
+                        x="15"
+                        y="15"
+                        width="70"
+                        height="70"
+                        stroke="red"
+                        strokeWidth="2"
+                        fill="yellow"
+                    />
+                </Svg>
+            </View>
+        </NativeBaseProvider>
     );
 };
